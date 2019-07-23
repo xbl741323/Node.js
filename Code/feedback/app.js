79b07d06fab47b9c46d8e670f553f892dea47ac4
xbl <1546766776@qq.com>
2019-07-23 16:06:58 +0800

    打卡

--- Code/feedback/app.js
+++ Code/feedback/app.js
@@ -9,28 +9,87 @@
 var http = require('http')
 var fs = require('fs')
 var template = require('art-template')
-
+var url = require('url')
 var server = http.createServer()
 
+var comments = [
+    {
+        name: 'admin01',
+        message: 'meassage01',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin02',
+        message: 'meassage02',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin03',
+        message: 'meassage04',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin04',
+        message: 'meassage04',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin05',
+        message: 'meassage05',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin06',
+        message: 'meassage06',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin07',
+        message: 'meassage07',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin08',
+        message: 'meassage08',
+        dateTime: '2019-7-17'
+    },
+
+]
+// /pinglun?name=的撒的撒&message=的撒的撒的撒
+// 对于这种表单提交的请求路径，由于其中具有用户动态填写的内容
+// 所以你不可能通过去判断完整的 url 路径来处理这个请求
+// 
+// 结论：对于我们来讲，其实只需要判定，如果你的请求路径是 /pinglun 的时候，那我就认为你提交表单的请求过来了
 server.on('request', function (req, res) {
-    var url = req.url
-    if (url === '/') {
+    // 使用 url.parse 方法将路径解析为一个方便操作的对象，第二个参数为 true 表示直接将查询字符串转为一个对象（通过 query 属性来访问）
+    var parseObj = url.parse(req.url, true)
+
+    // 单独获取不包含查询字符串的路径部分（该路径不包含 ? 之后的内容）
+    var pathname = parseObj.pathname
+    if (pathname === '/') {
         fs.readFile('./view/index.html', function (err, data) {
             if (err) {
                 return res.end('文件读取失败！')
             }
+
+            var htmlRet = template.render(data.toString(), {
+                comments: comments
+            })
             res.setHeader('Content-Type', 'text/html; charset=utf-8')
-            res.end(data)
+            res.end(htmlRet)
         })
-    } else if (url.indexOf('/public/') === 0) {
-        fs.readFile('.' + url, function (err, data) {
+    } else if (pathname.indexOf('/public/') === 0) {
+        // 统一处理：
+        //    如果请求路径是以 /public/ 开头的，则我认为你要获取 public 中的某个资源
+        //    所以我们就直接可以把请求路径当作文件路径来直接进行读取
+        fs.readFile('.' + pathname, function (err, data) {
             if (err) {
                 res.setHeader('Content-Type', 'text/plain; charset=utf-8')
                 return res.end('文件读取失败！')
             }
             res.end(data)
         })
-    } else if (url === '/post') {
+    } else if (pathname === '/post') {
         fs.readFile('./view/post.html', function (err, data) {
             if (err) {
                 res.setHeader('Content-Type', 'text/plain; charset=utf-8')
@@ -38,6 +97,28 @@ server.on('request', function (req, res) {
             }
             res.end(data)
         })
+    } else if (pathname === '/pinglun') {
+        // 我们已经使用 url 模块的 parse 方法把请求路径中的查询字符串给解析成一个对象了
+        // 所以接下来要做的就是：
+        //    1. 获取表单提交的数据 parseObj.query
+        //    2. 将当前时间日期添加到数据对象中，然后存储到数组中
+        //    3. 让用户重定向跳转到首页 /
+        //       当用户重新请求 / 的时候，我数组中的数据已经发生变化了，所以用户看到的页面也就变了
+        var comment = parseObj.query
+        var date = new Date()
+        comment.dateTime = date.getFullYear()+"-"+parseInt(date.getMonth()+1)+"-"+date.getDay()
+        // 服务端这个时候已经把数据存储好了，接下来就是让用户重新请求 / 首页，就可以看到最新的留言内容了
+        comments.unshift(comment)
+        // 如何通过服务器让客户端重定向？
+        //    1. 状态码设置为 302 临时重定向
+        //        statusCode
+        //    2. 在响应头中通过 Location 告诉客户端往哪儿重定向
+        //        setHeader
+        // 如果客户端发现收到服务器的响应的状态码是 302 就会自动去响应头中找 Location ，然后对该地址发起新的请求
+        // 所以你就能看到客户端自动跳转了
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        res.end()
     } else {
         fs.readFile('./view/404.html', function (err, data) {
             if (err) {
---
+++ Code/node16-url模块.js
@@ -0,0 +1,5 @@
+var url = require('url')
+
+var obj = url.parse('/pinglun?name=张三&message=今天好热')
+
+console.log(obj)
