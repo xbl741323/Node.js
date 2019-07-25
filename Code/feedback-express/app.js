bdee9a0dbe98ee573cf0cef154873cf8e3283ea1
xbl <1546766776@qq.com>
2019-07-25 13:29:56 +0800

    打卡

--- Code/feedback-express/app.js
+++ Code/feedback-express/app.js
@@ -1,13 +1,86 @@
 var express = require('express')
-
+var bodyParser = require('body-parser')
 var app = express()
+app.use('/public/', express.static('./public/'))
+// 配置使用 art-template 模板引擎
+app.engine('html', require('express-art-template'))
 
-app.use('/public/',express.static('./public/'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/',function(req,res){
-    res.send('hello express')
+var comments = [
+    {
+        name: 'admin01',
+        message: 'meassage01',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin02',
+        message: 'meassage02',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin03',
+        message: 'meassage04',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin04',
+        message: 'meassage04',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin05',
+        message: 'meassage05',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin06',
+        message: 'meassage06',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin07',
+        message: 'meassage07',
+        dateTime: '2019-7-17'
+    },
+    {
+        name: 'admin08',
+        message: 'meassage08',
+        dateTime: '2019-7-17'
+    },
+
+]
+// 如果想要修改默认的 views 目录，则可以
+// app.set('views', render函数的默认路径)
+app.get('/', function (req, res) {
+    res.render('index.html', { // express 默认会去 views 目录中找 index.html
+        comments: comments
+    })
+})
+// 担当路由（映射关系）的作用
+app.get('/post', function (req, res) {
+    res.render('post.html', {
+        comments: comments
+    })
 })
 
-app.listen(3300,function(){
+app.post('/pinglun', function (req, res) {
+    // req.query 只能拿 get 请求参数
+    var comment = req.body 
+    var date = new Date()
+    comment.dateTime = date.getFullYear() + "-" + parseInt(date.getMonth() + 1) + "-" + date.getDay()
+    comments.unshift(comment)
+    res.redirect('/') // express 封装的重定向方法
+})
+// app.get('/pinglun', function (req, res) {
+//     var comment = req.query 
+//     var date = new Date()
+//     comment.dateTime = date.getFullYear() + "-" + parseInt(date.getMonth() + 1) + "-" + date.getDay()
+//     comments.unshift(comment)
+//     res.redirect('/')
+// })
+
+app.listen(3300, function () {
     console.log('Server is running ...')
 })
